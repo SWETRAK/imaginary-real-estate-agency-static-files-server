121e0a6a5fc42fbd32ae9ed14ba9ff3e7eca9166
Kamil Pietrak <kamilpietrak123@gmail.com>
2022-05-23 11:45:29 +0200

    Added form validation

--- static/js/offerForm.js
+++ static/js/offerForm.js
@@ -1,10 +1,18 @@
+let emailError = false;
+let titleError = false;
+let addressError = false;
+let priceError = false;
+let areaError = false;
+let descriptionError = false;
+
+
 document.getElementById("front_photo").onchange = () => {
     const frontImageInput = document.getElementById("front_photo");
     const frontImageLabel = document.getElementById("front_photo_label");
     if(frontImageInput.files.length > 0) {
         frontImageLabel.textContent = frontImageInput.files[0].name;
     } else {
-        frontImageLabel.textContent = "Doesn't selected"
+        frontImageLabel.textContent = "Don't selected"
     }
 }
 
@@ -14,55 +22,147 @@ document.getElementById("add_photo1").onchange = () => {
     if(addImage1.files.length > 0) {
         addImage1Label.textContent = addImage1.files[0].name;
     } else {
-        addImage1Label.textContent = "Doesn't selected";
+        addImage1Label.textContent = "Don't selected";
     }
 }
 
 document.getElementById("add_photo2").onchange = () => {
     const addImage2 = document.getElementById("add_photo2");
-    const addImage2Label = document.getElementById("add_photo2_label");
+    const addImage2Label = document.getElementById("add_image2_label");
     if(addImage2.files.length > 0) {
         addImage2Label.textContent = addImage2.files[0].name;
     } else {
-        addImage2Label.textContent = "Doesn't selected";
+        addImage2Label.textContent = "Don't selected";
     }
 
 }
 
 document.getElementById("add_photo3").onchange = () => {
     const addImage3 = document.getElementById("add_photo3");
-    const addImage3Label = document.getElementById("add_photo3_label");
+    const addImage3Label = document.getElementById("add_image3_label");
     if(addImage3.files.length > 0) {
         addImage3Label.textContent = addImage3.files[0].name;
     } else {
-        addImage3Label.textContent = "Doesn't selected";
+        addImage3Label.textContent = "Don't selected";
     }
 
 }
 
 document.getElementById("add_photo4").onchange = () => {
     const addImage4 = document.getElementById("add_photo4");
-    const addImage4Label = document.getElementById("add_photo4_label");
+    const addImage4Label = document.getElementById("add_image4_label");
     if(addImage4.files.length > 0) {
         addImage4Label.textContent = addImage4.files[0].name;
     } else {
-        addImage4Label.textContent = "Doesn't selected";
+        addImage4Label.textContent = "Don't selected";
     }
 
 }
 
 document.getElementById("add_photo5").onchange = () => {
     const addImage5 = document.getElementById("add_photo5");
-    const addImage5Label = document.getElementById("add_photo5_label");
+    const addImage5Label = document.getElementById("add_image5_label");
     if(addImage5.files.length > 0) {
         addImage5Label.textContent = addImage5.files[0].name;
     } else {
-        addImage5Label.textContent = "Doesn't selected";
+        addImage5Label.textContent = "Don't selected";
+    }
+}
+
+document.getElementById("title").onchange = () => {
+    const title = document.getElementById("title").value;
+    const titleLabel = document.getElementById("title-message");
+    if( title.length === 0) {
+        titleLabel.classList.remove("is-hidden");
+        titleError = true;
+    } else {
+        titleLabel.classList.add("is-hidden");
+        titleError = false;
+    }
+}
+
+document.getElementById("address").onchange = () => {
+    const address = document.getElementById("address").value;
+    const addressLabel = document.getElementById("address-message");
+    if( address.length === 0) {
+        addressLabel.classList.remove("is-hidden");
+        addressError = true;
+    } else {
+        addressLabel.classList.add("is-hidden");
+        addressError = false;
+    }
+}
+
+document.getElementById("price").onchange = () => {
+    const price = document.getElementById("price").value;
+    const priceLabel = document.getElementById("price-message");
+    if( price.length === 0) {
+        priceLabel.classList.remove("is-hidden");
+        priceError = true;
+    } else {
+        priceLabel.classList.add("is-hidden");
+        priceError = false;
+    }
+}
+
+document.getElementById("area").onchange = () => {
+    const area = document.getElementById("area").value;
+    const areaLabel = document.getElementById("area-message");
+    if( area.length === 0) {
+        areaLabel.classList.remove("is-hidden");
+        areaError = true;
+    } else {
+        areaLabel.classList.add("is-hidden");
+        areaError = false;
+    }
+}
+
+document.getElementById("description").onchange = () => {
+    const description = document.getElementById("description").value;
+    const descriptionLabel = document.getElementById("description-message");
+    if( description.length === 0) {
+        descriptionLabel.textContent = "Description field can't be empty";
+        descriptionLabel.classList.remove("is-hidden");
+        descriptionError = true;
+    } else {
+        descriptionLabel.classList.add("is-hidden");
+        descriptionError = false;
+    }
+
+    if( description.length > 2048) {
+        descriptionLabel.textContent = "Description field max text length is 2048. Your text is to long.";
+        descriptionLabel.classList.remove("is-hidden");
+        descriptionError = true;
+    } else {
+        descriptionLabel.classList.add("is-hidden");
+        descriptionError = false;
+    }
+}
+
+
+document.getElementById("email").onchange = () => {
+    const email = document.getElementById("email").value
+    const emailLabel = document.getElementById("email-message");
+    if(!String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        ) || email.length === 0) {
+        emailLabel.classList.remove("is-hidden");
+        emailError = true;
+    } else {
+        emailLabel.classList.add("is-hidden");
+        emailError = false;
     }
 }
 
 document.getElementById("submit_button").addEventListener("click", (event) => {
     event.preventDefault();
+
+    if(emailError || areaError || descriptionError || titleError || priceError || addressError ) {
+        notificationWarning("Inserted values are incorrect. Correct them and try again.");
+        return;
+    }
 
     const title = document.getElementById("title").value;
     const address = document.getElementById("address").value;
@@ -72,10 +172,8 @@ document.getElementById("submit_button").addEventListener("click", (event) => {
     const bathrooms = parseFloat(document.getElementById("bathrooms").value);
     const description = document.getElementById("description").value;
     const email = document.getElementById("email").value;
-
     const terms = document.getElementById("terms").checked;
-
-
+    // IMAGES
     const frontImageInput = document.getElementById("front_photo");
     const addImage1 = document.getElementById("add_photo1");
     const addImage2 = document.getElementById("add_photo2");
@@ -83,7 +181,8 @@ document.getElementById("submit_button").addEventListener("click", (event) => {
     const addImage4 = document.getElementById("add_photo4");
     const addImage5 = document.getElementById("add_photo5");
 
-    if(isNaN(price) || isNaN(area) || isNaN(bedrooms) || isNaN(bathrooms) || title.length === 0 || address.length === 0 || description.length === 0 || email.length === 0 ) {
+
+    if(isNaN(price) || isNaN(area) || isNaN(bedrooms) || isNaN(bathrooms) ) {
         notificationWarning("Inserted values are incorrect. Correct them and try again.");
         return;
     }
@@ -93,7 +192,6 @@ document.getElementById("submit_button").addEventListener("click", (event) => {
         return;
     }
 
-    // TODO: Add validation
     const formdata = new FormData();
     formdata.append("title", title);
     formdata.append("email", email);
@@ -104,6 +202,7 @@ document.getElementById("submit_button").addEventListener("click", (event) => {
     formdata.append("bathrooms", bathrooms);
     formdata.append("description", description);
     formdata.append("terms", terms);
+
 
     if(frontImageInput.files.length === 0) {
         notificationWarning("Please select front offer file");
@@ -116,20 +215,21 @@ document.getElementById("submit_button").addEventListener("click", (event) => {
     }
 
     if(addImage2.files.length === 1) {
-        formdata.append("add_photo2", addImage2.files[0], addImage2.files[0].name);
+        formdata.append("add_photo1", addImage2.files[0], addImage2.files[0].name);
     }
 
     if(addImage3.files.length === 1) {
-        formdata.append("add_photo3", addImage3.files[0], addImage3.files[0].name);
+        formdata.append("add_photo1", addImage3.files[0], addImage3.files[0].name);
     }
 
     if(addImage4.files.length === 1) {
-        formdata.append("add_photo4", addImage4.files[0], addImage4.files[0].name);
+        formdata.append("add_photo1", addImage4.files[0], addImage4.files[0].name);
     }
 
     if(addImage5.files.length === 1) {
-        formdata.append("add_photo5", addImage5.files[0], addImage5.files[0].name);
+        formdata.append("add_photo1", addImage5.files[0], addImage5.files[0].name);
     }
+
 
     let requestOptions = {
         method: 'POST',
@@ -137,17 +237,16 @@ document.getElementById("submit_button").addEventListener("click", (event) => {
         redirect: 'follow'
     };
 
-    fetch("https://irea-app.herokuapp.com/api/v1/create", requestOptions)
+    fetch("http://localhost:8080/api/v1/create", requestOptions)
         .then(response => response.text())
         .then(result => {
-            // TODO: Sprawdzanie ciała result aby zobaczyć czy jest odpowiedni wynik requesta
+            console.log(result);
             notificationSuccess("Success. Your offer is ready");
         })
         .catch(error => {
             notificationDanger("Ups. Something went wrong. Try later or consult with your network administrator");
 
         });
-
 })
 
 
