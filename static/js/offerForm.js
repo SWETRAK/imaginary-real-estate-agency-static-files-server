bbc02322e0d3e628d5cb3b102759c3cb6ee867fe
Kamil Pietrak <kamilpietrak123@gmail.com>
2022-05-23 12:01:26 +0200

    Added form validation

--- static/js/offerForm.js
+++ static/js/offerForm.js
@@ -237,7 +237,7 @@ document.getElementById("submit_button").addEventListener("click", (event) => {
         redirect: 'follow'
     };
 
-    fetch("http://localhost:8080/api/v1/create", requestOptions)
+    fetch("https://irea-app.herokuapp.com/api/v1/create", requestOptions)
         .then(response => response.text())
         .then(result => {
             console.log(result);
